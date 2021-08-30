c754f3e139d87479e1397f2a4f2cca363ad1fd0c
Nikita Izmailov <nizmaylov13@gmail.com>
2021-08-31 02:00:15 +0300

    Final Final

--- myfirstapp/settings.py
+++ myfirstapp/settings.py
@@ -57,12 +57,15 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    #'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
 
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
